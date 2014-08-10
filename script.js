8644e867038311785fee20de5a08bd1a2c4ea385
crywolfe <gwolfe2007@gmail.com>
2014-08-09 22:18:22 -0500

    semicolons

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ var tipRate = prompt("What is the percentage of the desired tip rate?") / 100;
 var amount = function(baseCost, rate){
   var total = baseCost * rate;
   return parseFloat(total.toFixed(2));
-}
+};
 
 
 var calcTip = function(baseCost, amount) {
   var total = (baseCost + amount(baseCost, taxRate) +  amount(baseCost, tipRate));
   return total.toFixed(2);
-}
+};
