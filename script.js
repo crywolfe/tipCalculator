eb35a4165b5f714a98f57708102a0134e34584ba
crywolfe <gwolfe2007@gmail.com>
2014-08-09 21:53:16 -0500

    finish tipCalculator refactor

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var baseCost = parseFloat(prompt("What is the base cost of the meal?"));
+var taxRate = prompt("What is the tax rate in percentage?") / 100;
+var tipRate = prompt("What is the percentage of the desired tip rate?") / 100;
+
+var amount = function(baseCost, rate){
+  var total = baseCost * rate;
+  return parseFloat(total.toFixed(2));
+}
+
+
+var calcTip = function(baseCost, amount) {
+  debugger;
+  var total = (baseCost + amount(baseCost, taxRate) +  amount(baseCost, tipRate));
+  return total.toFixed(2);
+}
