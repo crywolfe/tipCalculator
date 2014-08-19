a95bc02f00cd81efb114dae4810621537544b69b
crywolfe <gwolfe2007@gmail.com>
2014-08-18 20:14:43 -0500

    object issues

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+var calculator = {
+  baseCost: parseFloat(prompt("What is the base cost of the meal?")),
+  taxRate: prompt("What is the tax rate in percentage?") / 100,
+  tipRate: prompt("What is the percentage of the desired tip rate?") / 100,
+  amount: function(baseCost, rate){
+    var total = baseCost * rate;
+    return parseFloat(total.toFixed(2));
+  },
+  calcTip: function(baseCost, amount) {
+    var total = (baseCost + amount(baseCost, taxRate) +  amount(baseCost, tipRate));
+    return total.toFixed(2);
+  }
 
-var baseCost = parseFloat(prompt("What is the base cost of the meal?"));
-var taxRate = prompt("What is the tax rate in percentage?") / 100;
-var tipRate = prompt("What is the percentage of the desired tip rate?") / 100;
 
-var amount = function(baseCost, rate){
-  var total = baseCost * rate;
-  return parseFloat(total.toFixed(2));
-};
-
-
-var calcTip = function(baseCost, amount) {
-  var total = (baseCost + amount(baseCost, taxRate) +  amount(baseCost, tipRate));
-  return total.toFixed(2);
-};
+}
